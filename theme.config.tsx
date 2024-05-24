7f9d092637d787acd65db0733033ef568c0d8d23
Ezra Lazuardy <ezralazuardy@icloud.com>
2024-05-25 00:12:18 +0700

    feat: update

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,63 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import Image from "next/image";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – Heimdall",
+    };
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Heimdall" />
+      <meta
+        property="og:description"
+        content="🔥 Painless OAuth 2.0 Server for CodeIgniter 4."
+      />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+    </>
+  ),
+  logo: (
+    <>
+      <span>🤺 Heimdall</span>
+    </>
+  ),
   footer: {
-    text: 'Nextra Docs Template',
+    component: (
+      <div className="footer">
+        <div className="footer-content-left">
+          <span>
+            written with ❤️ by{" "}
+            <b>
+              <a href="https://nextra.site" target="_blank">
+                nextra
+              </a>
+            </b>
+          </span>
+        </div>
+        <div className="footer-content-right">
+          <span>
+            copyright {new Date().getFullYear()}
+            {", "}
+            <b>
+              <a href="https://about.lazuardy.tech" target="_blank">
+                @ezralazuardy
+              </a>
+            </b>
+          </span>
+        </div>
+      </div>
+    ),
   },
-}
+  sidebar: {
+    toggleButton: true,
+  },
+  project: {
+    link: "https://github.com/ezralazuardy/heimdall",
+  },
+  docsRepositoryBase: "https://github.com/ezralazuardy/heimdall-docs",
+};
 
-export default config
+export default config;
